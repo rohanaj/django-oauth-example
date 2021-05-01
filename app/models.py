39f0738e24f530cb155e97bc3672ff02d9f2c1db
ROHAN AJMANI <13540751+rohanaj@users.noreply.github.com>
2021-05-01 10:53:48 +0530

    Update models.py
    correction

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class CartProduct(models.Model):
     product = models.ForeignKey('Products',on_delete = models.CASCADE)
     qty = models.IntegerField()
     def __str__(self):
-        return self.pk
+        return str(self.pk)
 
 
 
